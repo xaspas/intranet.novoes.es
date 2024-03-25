bdb42ee27209c33936d911c8b94380bcded9b565
xaspas <xaspas@EOL-CP001>
2024-03-25 15:52:11 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace intranet.novoes.es.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
